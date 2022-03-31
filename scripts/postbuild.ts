4b8e3bb1ff1b91683cb832506021343e6cbc2e82
chris-zhu <1633711653@qq.com>
2022-03-31 14:17:50 +0800

    feat: update build script

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -3,23 +3,19 @@ import { promises as fs } from 'fs'
 import fg from 'fast-glob'
 
 async function run() {
+  // fix cjs exports
   const files = await fg('*.js', {
-    ignore: ['chunk-*'],
+    ignore: ['index.js', 'chunk-*'],
     absolute: true,
     cwd: resolve(__dirname, '../dist'),
   })
   for (const file of files) {
     // eslint-disable-next-line no-console
-    if (file === 'index.js') {
-      // fix cjs exports
-      let code = await fs.readFile(file, 'utf8')
-      code = code.replace('exports.default =', 'module.exports =')
-      code += 'exports.default = module.exports;'
-      await fs.writeFile(file, code)
-    }
-    // generate submodule .d.ts redirecting
-    const name = basename(file, '.js')
-    await fs.writeFile(`${name}.d.ts`, `export { default } from './dist/${name}'\n`)
+    console.log('[postbuild]', basename(file))
+    let code = await fs.readFile(file, 'utf8')
+    code = code.replace('exports.default =', 'module.exports =')
+    code += 'exports.default = module.exports;'
+    await fs.writeFile(file, code)
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { createUnplugin } from 'unplugin'
 import { MODULE_NAME } from './core/constants'
 import Context from './core/context'
 import type { Options } from './types'
+import type { ResolvedConfig, ViteDevServer } from 'vite'
 
 export default createUnplugin<Options>((options) => {
   const ctx = new Context(options)
 
   return {
     name: MODULE_NAME,
+    enforce: 'post',
     // transformInclude(id) {
     //   return id.endsWith('main.ts')
     // },
@@ -17,15 +19,15 @@ export default createUnplugin<Options>((options) => {
       return result
     },
     vite: {
-      // configResolved(config: any) {
-      //   ctx.setRoot(config.root)
+      configResolved(config: ResolvedConfig) {
+        ctx.setRoot(config.root)
 
-      //   if (options?.dts)
-      //     ctx.generateDeclaration()
-      // },
-      // configureServer(server: any) {
-      //   ctx.setupViteServer(server)
-      // },
+        if (options?.dts)
+          ctx.generateDeclaration()
+      },
+      configureServer(server: ViteDevServer) {
+        ctx.setupViteServer(server)
+      },
     },
   }
 })
